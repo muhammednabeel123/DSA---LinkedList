c317d7a199f8604e2ff25ac209bcda770958d58b
muhammednabeel123 <muhammednabeel237@gmail.com>
2023-05-16 14:52:03 +0530

    string methods

--- stringMethods.js
+++ stringMethods.js
@@ -5,20 +5,41 @@ let result = text1.concat(" ",text2 )
 console.log(result)
 
 //string charAt
-console.log(text1.charAt(0))
+// console.log(text1.charAt(0))
 
 //charCodeAt 
-console.log(text1.charCodeAt(2))
+// console.log(text1.charCodeAt(2))
 
 //endsWith
 let text3 = "hello world"
-console.log(text3.endsWith("d"))
+
 
 //formCode
-console.log(String.fromCharCode(77))
+// console.log(String.fromCharCode(77))
 
 //localeCompare
 let text4 = "Ab"
 let text5 = "Ab"
 
 console.log(text4.localeCompare(text5))
+
+//finad all 
+function string(target){
+let text6 = "banana"
+let count = 0
+
+for(let i = 0;i<text6.length;i++){
+    console.log(text6[i]);
+
+    if(text6[i] == target.toString() ){
+   
+        count ++;
+      
+    }
+
+   
+}
+return count;
+}
+
+//splid()
